498df304edd7d486b1925b7bcfeeda31935b0cdc
Pavel Stepanov <pavelstepanov@vivaldi.net>
2021-07-20 16:56:22 +0600

    arrange files for proper import

--- default.nix
+++ default.nix
@@ -1,54 +1,51 @@
-# Copyright 2019 Bradley Jensen
-#
-# Permission is hereby granted, free of charge, to any person obtaining
-# a copy of this software and associated documentation files (the
-# "Software"), to deal in the Software without restriction, including
-# without limitation the rights to use, copy, modify, merge, publish,
-# distribute, sublicense, and/or sell copies of the Software, and to
-# permit persons to whom the Software is furnished to do so, subject to
-# the following conditions:
-#
-# The above copyright notice and this permission notice shall be
-# included in all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+{ pkgs, dependencies ? [], quicklisp-packages ? [], dists ? [] }:
+distfile:
+  
+with pkgs;
 
-{ pkgs ? import <nixpkgs> {} }:
 let
-  build = pkgs.writeTextFile {
-    name = "ql2nix-build.lisp";
-    text = ''
-      (require '#:asdf)
-      (let* ((asdf:*central-registry* (cons (truename ".") asdf:*central-registry*))
-             (system (asdf:find-system '#:ql2nix))
-             (uiop:*image-dump-hook* (cons (lambda () (asdf:clear-system system)) uiop:*image-dump-hook*)))
-        (asdf:oos 'asdf:program-op system))
-    '';
-  };
-  clwrapper = pkgs.lispPackages.clwrapper;
-in
-pkgs.stdenv.mkDerivation rec {
-  name = "ql2nix-${version}";
-  version = "1.0.0";
-  src = ./.;
-  buildInputs = [
-    clwrapper
-  ];
-  buildPhase = ''
-    ASDF_OUTPUT_TRANSLATIONS="(:output-translations :ignore-inherited-configuration (t \"$(pwd)\"))"
-    export ASDF_OUTPUT_TRANSLATIONS
-    NIX_LISP_SKIP_CODE=1 source "${clwrapper}/bin/common-lisp.sh" || true
-    "${clwrapper}/bin/common-lisp.sh" "$NIX_LISP_LOAD_FILE" "${build}"
+  ql2nix-src = lib.cleanSource ./ql2nix;
+  build = writeScript "build.lisp" ''
+    ;; Setup quicklisp
+    (load "quicklisp.lisp")
+    (quicklisp-quickstart:install)
+
+    ;; Load ql2nix from source
+    (load "${ql2nix-src}/main.lisp")
+
+    (dolist (dist '(${
+      lib.concatMapStringsSep " " (x: ''"'' + x + ''"'') dists
+    }))
+      (ql-dist:install-dist dist :prompt nil))
+
+    ;; Generate dependency definitions
+    (ql2nix:main (list "--nixlisp-lib" ${
+      lib.concatMapStringsSep " " (x: ''"'' + x + ''"'') quicklisp-packages
+    }))
+    (quit)
   '';
-  installPhase = ''
-    mkdir -p "$out/bin"
-    cp ql2nix "$out/bin"
+  generate-dist = writeScriptBin "generate-dist" ''
+    set -e
+    # Remember output directory
+    out=$(pwd)/
+
+    [ ! -d "$out" ] && mkdir -p "$out"
+
+    # Create temporary home directory to work in
+    tmp=$(mktemp -d --suffix=.boot.ql2nix)
+    trap "rm -rf $tmp" EXIT
+    cd $tmp
+    HOME=$(pwd)
+
+    # Install Quicklisp and run ql2nix
+    curl -O https://beta.quicklisp.org/quicklisp.lisp
+    bash -x $(which common-lisp.sh) --load ${build}
+
+    # Copy generated files into output directory
+    echo "Copying generated files"
+    cp --no-preserve=mode --remove-destination -v -f qlDist.nix $out/
   '';
-  dontStrip = true;
-}
+  mkNixlispBundle = import ./mkNixlispBundle.nix pkgs;
+  bundle = mkNixlispBundle dependencies distfile;
+  # Derivation defining an environment for use with nix-shell.
+in with pkgs; [ bundle ] ++ dependencies ++ [ curl lispPackages.clwrapper generate-dist ]
