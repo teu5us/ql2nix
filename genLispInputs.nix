e5345aaba7ed527620e76af890d1193589ecf8ec
Pavel Stepanov <pavelstepanov@vivaldi.net>
2021-07-20 20:39:10 +0600

    add local projects to registry

--- genLispInputs.nix
+++ genLispInputs.nix
@@ -1,10 +1,11 @@
-{ pkgs, dependencies ? [], quicklisp-packages ? [], dists ? [] }:
+{ pkgs, dependencies ? [], quicklisp-packages ? [], dists ? [], trees ? [], dirs ? [] }:
 distfile:
   
 with pkgs;
 
 let
   ql2nix-src = lib.cleanSource ./ql2nix;
+  concatStringList = lst: lib.concatMapStringsSep " " (x: ''"'' + x + ''"'') lst;
   build = writeScript "build.lisp" ''
     ;; Setup quicklisp
     (load "quicklisp.lisp")
@@ -13,15 +14,11 @@ let
     ;; Load ql2nix from source
     (load "${ql2nix-src}/main.lisp")
 
-    (dolist (dist '(${
-      lib.concatMapStringsSep " " (x: ''"'' + x + ''"'') dists
-    }))
+    (dolist (dist '(${concatStringList dists}))
       (ql-dist:install-dist dist :prompt nil))
 
     ;; Generate dependency definitions
-    (ql2nix:main (list "--nixlisp-lib" ${
-      lib.concatMapStringsSep " " (x: ''"'' + x + ''"'') quicklisp-packages
-    }))
+    (ql2nix:main (list "--nixlisp-lib" ${concatStringList quicklisp-packages}))
     (quit)
   '';
   generate-dist = writeScriptBin "generate-dist" ''
@@ -45,7 +42,26 @@ let
     echo "Copying generated files"
     cp --no-preserve=mode --remove-destination -v -f qlDist.nix $out/
   '';
+  load-trees = writeScript "load-trees" ''
+    (defun fix-dirname (str)
+      (let ((slash? (equal #\/ (aref (reverse str) 0))))
+        (if slash?
+            str
+            (concatenate 'string str "/"))))
+
+    (dolist (tree (list ${concatStringList trees}))
+      (let* ((tree- (fix-dirname tree)))
+        (dolist (dir (uiop:subdirectories tree-))
+          (pushnew dir asdf:*central-registry*))))
+
+    (dolist (dir (list ${concatStringList dirs}))
+      (pushnew (fix-dirname dir) asdf:*central-registry*))
+  '';
+  lisp = writeScriptBin "lisp" ''
+    #!${bash}/bin/bash
+    common-lisp.sh --load ${load-trees}
+  '';
   mkNixlispBundle = import ./ql2nix/mkNixlispBundle.nix pkgs;
   bundle = mkNixlispBundle dependencies distfile;
   # Derivation defining an environment for use with nix-shell.
-in with pkgs; [ bundle ] ++ dependencies ++ [ curl lispPackages.clwrapper generate-dist ]
+in with pkgs; [ bundle ] ++ dependencies ++ [ curl lispPackages.clwrapper generate-dist lisp ]
